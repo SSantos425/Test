701e3c5f72f1839e6cacd7559198aefa7b994b4f
SSantos425 <samuel.tri@hotmail.com>
2020-05-04 19:30:23 -0300

    Projeto criado

---
+++ src/application/Test.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int vector[] = { 1, 2, 3, 4, 5 };
+		for (int i = 0; i < vector.length; i++) {
+			System.out.print(vector[i] + " ");
+		}
+		System.out.println();
+		System.out.println("Typer a number to be removed");
+		int number=sc.nextInt();
+		for (int i = 0; i < vector.length-1; i++) {
+			if(vector[i]==number) {
+				vector[i]=vector[i--];
+			}
+		}
+		System.out.println();
+		for (int i = 0; i < vector.length; i++) {
+			System.out.print(vector[i] + " ");
+		}
+		sc.close();
+	}
+
+	
+}
